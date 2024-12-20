464a53ed24a74bd78a9fe1312d6c07ea231cabe6
Andy Couto <Andrea.Couto@panerabread.com>
2024-12-20 14:09:34 -0500

    initial xcode changes

---
+++ Mood Tracker/Mood Tracker/Mood_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_TrackerApp.swift
+//  Mood Tracker
+//
+//  Created by Andy Couto on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Mood_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
